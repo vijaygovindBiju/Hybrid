f9d2c75df68d49c1fdb43e00d5a04f86b6aac3ca
vijaygovindBiju <vijaygovindbiju@gmail.com>
2026-07-24 19:32:11 +0530

    created provider and try to disply the list

---
+++ lib/features/get_song/data/provider/getSong_prov.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hybrid/features/get_song/function/fetch_songs.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+
+final getSongProvider = FutureProvider<List<SongModel>>((ref) async {
+  List<SongModel> songs = await FetchSongs().getSong();
+  return songs;
+});
--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:hybrid/features/get_song/function/fetch_songs.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hybrid/features/get_song/Presentation/widget/show_song.dart';
+import 'package:hybrid/features/get_song/data/provider/getSong_prov.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
-class Home extends StatelessWidget {
-   Home({super.key});
-  final call = FetchSongs();
+class Home extends ConsumerStatefulWidget {
+  const Home({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends ConsumerState<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: ElevatedButton(onPressed: call.getSong, child: Text("Get")),
+    return Expanded(
+      child: Column(
+        children: [
+          switch (ref.watch(getSongProvider)) {
+            AsyncError() => const Text("Sorry can't fetch song"),
+            AsyncData(:final value) => Expanded(
+              child: ListView.builder(
+                itemCount: value.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ShowSong(
+                    id: value[index].id,
+                    uri: value[index].uri,
+                    data: value[index].data,
+                    displayName: value[index].displayName,
+                    displayNameWOExt: value[index].displayNameWOExt,
+                    size: value[index].size,
+                    album: value[index].album,
+                    albumId: value[index].albumId,
+                    artist: value[index].artist,
+                  );
+                },
+              ),
+            ),
+            _ => CircularProgressIndicator(),
+          },
+        ],
       ),
     );
   }
